044cda3d063f222185706ae69584ba446b1bc658
jhs512 <jangka512@gmail.com>
2025-07-02 10:33:05 +0900

    step 2

--- src/app/posts/write/page.tsx
+++ src/app/posts/write/page.tsx
@@ -40,7 +40,7 @@ export default function Page() {
       }),
     }).then((data) => {
       alert(data.msg);
-      router.push(`/posts/${data.data.id}`);
+      router.replace(`/posts/${data.data.id}`);
     });
   };
 
